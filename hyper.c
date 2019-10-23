9d49021ff8c49958cf9098aad5fd935c89eade9d
Jimmy Le <jimmyle77@hotmail.com>
2019-10-23 00:13:04 -0400

    added hardcoded processor picking

--- hyper.c
+++ hyper.c
@@ -30,10 +30,12 @@ char* to_binary(int n, int length)
 int main(int argc, char* argv[])
 {
     
-    int num_tasks, task_id, size, degree = 3, pivot;
+    int num_tasks, task_id,child_id, size, degree = 3, pivot, color;
     int recv_data[10];
 
     MPI_Init(&argc, &argv);
+
+    MPI_Comm childcomm;
 
     MPI_Comm_size(MPI_COMM_WORLD, &num_tasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &task_id);
@@ -42,19 +44,20 @@ int main(int argc, char* argv[])
     /*Scatter parts of array to each process*/
     if (task_id == 0)
     {
-        int n[10] = { 25, 16, 4, 1,64,81,100,49,36,9 };
+        int n[16] = { 1,11,21,31,41,51,61,71,81,91,101,111,121,131,141,151 };
         size = (sizeof(n)/sizeof(int))/num_tasks;
    
         MPI_Bcast(&size, 1, MPI_INT, 0, MPI_COMM_WORLD);
         MPI_Scatter(n, size, MPI_INT, &recv_data, size, MPI_INT, 0, MPI_COMM_WORLD);
-
-        printf("Task %d:\t", task_id);
+        /*
+        printf("Task %d: in mpi_comm_world\t", task_id);
 
         for (int i = 0; i < size; i++)
         {
             if (recv_data[i] > 0)
                 printf("%d\t", recv_data[i]);
         }
+        */
     }
     else
     {
@@ -62,7 +65,7 @@ int main(int argc, char* argv[])
         MPI_Bcast(&size, 1, MPI_INT, 0, MPI_COMM_WORLD);
         MPI_Scatter(&n, 0, MPI_INT, &recv_data, size, MPI_INT, 0, MPI_COMM_WORLD);
 
-        printf("Task %d:\t", task_id);
+        //printf("Task %d in MPI_comm_world\t", task_id);
         /*
         for (int i = 0; i < size; i++)
         {
@@ -74,14 +77,14 @@ int main(int argc, char* argv[])
     /* END initial scattering*/
 
     
-    if (task_id == 0)
-        printf("\nFOR LOOP STARTS\n");
     /*
         figure out binary
         rank 0 bcast pivot
     */
-    for (int d = 0; d < 1; d++)
+    int max = 1;
+    for (int d = 0; d < max; d++)
     {
+        /*
         char* str;
         str = to_binary(task_id, degree);
         char * str_copy = to_binary(task_id, degree);
@@ -90,40 +93,100 @@ int main(int argc, char* argv[])
             str_copy[d] = '0';
         else
             str_copy[d] = '1';
-        // #TODO: get process to communicate with process that differs in specifically one bit
-
-        
-
-        printf(" %s and %s \n",str,str_copy);
-        
-
+        printf(" %s and %s \n", str, str_copy);
         printf("Task: %d binary[%d] : %c\n", task_id,d, str[d]);
 
-        if (task_id == 0)
+        */
+
+        if (d == 0)
         {
+
+            //comm split
+            if (task_id == 0 || task_id == 4)
+            {
+                color = 0;
+            }
+            else if (task_id == 1 || task_id == 5)
+            {
+                color = 1;
+            }
+            else if (task_id == 2 || task_id == 6)
+            {
+                color = 2;
+            }
+            else
+            {
+                color = 3;
+            }
+            MPI_Comm_split(MPI_COMM_WORLD, color, task_id, &childcomm);
+            //printf("comm_world rank:  %d", task_id);
+
+            MPI_Comm_rank(childcomm, &child_id);
+//            printf("Color:Rank in childcomm is  %d:%d\n",color, child_id);
+            /*
+                Split the comm
             
-            //printf("task_id: %d binary: %s\n", task_id, str);
+            */
+            int data[2];
 
-            pivot = (0+size)/2;
-            MPI_Bcast(&pivot, 1, MPI_INT, 0, MPI_COMM_WORLD);
-            //printf("pivot data is: %d\n", recv_data[pivot]);
+            if (child_id == 0)
+            {
+                MPI_Send(&recv_data[0], 1, MPI_INT, 1, 0, childcomm);
+                MPI_Send(&recv_data[1], 1, MPI_INT, 1, 0, childcomm);
+
+
+                MPI_Recv(&data[0], 1, MPI_INT, 1, 0, childcomm, MPI_STATUS_IGNORE);
+                MPI_Recv(&data[1], 1, MPI_INT, 1, 0, childcomm, MPI_STATUS_IGNORE);
+
+            }
+            else
+            {   
+                MPI_Recv(&data[0], 1, MPI_INT, 0, 0, childcomm, MPI_STATUS_IGNORE);
+                MPI_Recv(&data[1], 1, MPI_INT, 0, 0, childcomm, MPI_STATUS_IGNORE);
+
+                MPI_Send(&recv_data[0], 1, MPI_INT, 0, 0, childcomm);
+                MPI_Send(&recv_data[1], 1, MPI_INT, 0, 0, childcomm);
+
+            }
+            int working_arr[] = { data[0], data[1], recv_data[0], recv_data[1] };
+
+            if (child_id == 0)
+            {
+
+                //printf("task_id: %d binary: %s\n", task_id, str);
+
+                pivot = (sizeof(working_arr)/sizeof(int))/2;
+                MPI_Bcast(&pivot, 1, MPI_INT, 0, childcomm);
+
+            }
+            else
+            {
+
+                //printf("task_id: %d binary: %s\n", task_id, str);
+                pivot = 0;
+                MPI_Bcast(&pivot, 1, MPI_INT, 0, childcomm);
 
 
 
-            
+            }
+            printf("pivot data is: %d in ", working_arr[pivot]);
+            printf("arr = %d\t%d\t%d\t%d\n", data[0], data[1], recv_data[0], recv_data[1]);
+
+
+            /*
+                separate array into lower end and higher end
+                if smaller than pivot, 
+            */
         }
-        else
-        {
-            
-            //printf("task_id: %d binary: %s\n", task_id, str);
+        
 
-            MPI_Bcast(&pivot, 1, MPI_INT, 0, MPI_COMM_WORLD);
-            //printf("pivot data is: %d\n",recv_data[pivot]);
-
-        }
+        
 
 
-        free(str);
+        
+
+
+//        free(str);
     }
 
     MPI_Finalize();
