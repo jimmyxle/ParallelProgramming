d338f7ac14fc50e0869d4120179e94192e5b425c
Jimmy Le <jimmyle77@hotmail.com>
2019-11-25 12:51:50 -0500

    rand fill array

--- a3/bcast_floyd.c
+++ a3/bcast_floyd.c
@@ -48,76 +48,23 @@ int** init_array(int num)
 
 int** fill_array(int** adj, int num)
 {
-	for (int i = 0; i < num; i++)
-		insert_node(adj, i);
-
-    insert_edge(adj, 0, 1, 3);
-    insert_edge(adj, 0, 2, 4);
-    insert_edge(adj, 0, 3, 5);
-    insert_edge(adj, 0, 5, 6);
-
-    insert_edge(adj, 1, 2, 4);
-    insert_edge(adj, 1, 5, 5);
-    insert_edge(adj, 1, 6, 2);
-
-    insert_edge(adj, 2, 6, 1);
-    insert_edge(adj, 2, 3, 4);
-    insert_edge(adj, 2, 7, 3);
-    insert_edge(adj, 2, 8, 3);
-
-    insert_edge(adj, 3, 8, 1);
-    insert_edge(adj, 3, 9, 3);
-    insert_edge(adj, 3, 10, 1);
-    
-    insert_edge(adj, 4, 3, 7);
-    insert_edge(adj, 4, 10, 2);
-
-    insert_edge(adj, 5, 6, 2);
-    insert_edge(adj, 5, 11, 8);
-
-    insert_edge(adj, 6, 11, 4);
-    insert_edge(adj, 6, 7, 3);
-    insert_edge(adj, 6, 8, 6);
-
-    insert_edge(adj, 7, 11, 4);
-    insert_edge(adj, 7, 12, 5);
-    insert_edge(adj, 7, 13, 1);
-    insert_edge(adj, 7, 8, 9);
-
-    insert_edge(adj, 8, 9, 4);
-    insert_edge(adj, 8, 14, 4);
-    insert_edge(adj, 8, 13, 1);
-
-    insert_edge(adj, 9, 14, 9);
-    insert_edge(adj, 9, 15, 9);
-    insert_edge(adj, 9, 18, 7);
-
-    insert_edge(adj, 10, 9, 3);
-    insert_edge(adj, 10, 15, 1);
-
-    insert_edge(adj, 11, 12, 9);
-    insert_edge(adj, 11, 16, 1);
-
-    insert_edge(adj, 12, 13, 2);
-    insert_edge(adj, 12, 16, 3);
-    insert_edge(adj, 12, 17, 3);
-
-    insert_edge(adj, 13, 17, 1);
-    insert_edge(adj, 13, 19, 2);
-
-    insert_edge(adj, 14, 19, 9);
-    insert_edge(adj, 14, 18, 1);
-    insert_edge(adj, 14, 15, 3);
-
-    insert_edge(adj, 15, 18, 5);
-
-    insert_edge(adj, 16, 17, 2);
-
-    insert_edge(adj, 17, 19, 1);
-
-    insert_edge(adj, 18, 19, 4);
-    
-	return adj;
+    time_t t;
+    srand((unsigned)time(&t));
+    for (int i = 0; i < num; i++)
+        insert_node(adj, i);
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < num; j++)
+        {
+            int temp1 = rand() % (num);
+            int temp2 = rand() % (num);
+            if (temp1 != temp2)
+            {
+                insert_edge(adj, temp1, temp2, rand() % 10);
+            }
+        }
+    }
+    return adj;
 }
 
 int main(int argc, char* argv[])
